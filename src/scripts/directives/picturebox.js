d33df68db464323a2e5a81e5ac4c5430afe2cdc6
Jess Champion <jess.e.champion@gmail.com>
2016-09-07 11:40:20 +1200

    Picture viewer: auto zoom

--- src/scripts/directives/picturebox.js
+++ src/scripts/directives/picturebox.js
@@ -7,51 +7,139 @@
  * # PictureBox
  */
 angular.module('jessMakesThingsApp')
-.directive('pictureBox', function () {
+.directive('pictureBox', function ($compile) {
     return {
         restrict: 'EA',
         scope: {
             modalContent: '='
         },
-        controller: function ($scope, $timeout) {
-            $scope.pictureBox = {};
-            $scope.pictureBox.isOpen = false;
+        controller: function ($scope) {
+            var ZOOM_MAX = 10, ZOOM_MIN = -10;
+            $scope.pictureBox = {
+                isOpen: false,
+                zoom: 0,
+                image:{
+                    element: null,
+                    actual: {
+                        width: 0,
+                        height: 0
+                    },
+                    display: {
+                        width: 'auto',
+                        height: 'auto'
+                    }
+                },
+                wrapper:{
+                    element: null,
+                    width: 0,
+                    height: 0
+                }
+            };
+            var pictureBox = $scope.pictureBox;
 
-
-            $scope.pictureBox.close = function () {
+            pictureBox.close = function () {
                 console.log("click popover");
                 $('.picture-box.overlay').remove();
                 $('body').removeClass('picture-box-open');
-                $scope.pictureBox.isOpen = false;
+                pictureBox.isOpen = false;
             };
 
-            $scope.pictureBox.open = function () {
+            pictureBox.open = function () {
                 console.log("click picture");
                 var body = $('body');
-                body.append($scope.pictureBox.popover);
+                body.append(pictureBox.popover);
                 body.addClass('picture-box-open');
 
-                $('.picture-box.overlay').click(function () {
-                    $scope.pictureBox.close();
+                pictureBox.image.element = $('.image', pictureBox.popover);
+                pictureBox.wrapper.element = $('.image-wrapper', pictureBox.popover);
+
+                $(pictureBox.image.element).ready( function () {
+                    loadImageDimensions();
+                    loadWrapperDimensions();
+                    autoResize();
+                    $(pictureBox.wrapper.element).resize( function () {
+                        loadWrapperDimensions();
+                        if (pictureBox.zoom === 0){
+                            autoResize();
+                        }
+                    })
                 });
-                $('.picture-box .image-wrapper').click(function () {
-                    $scope.pictureBox.close();
+
+                $('.picture-box.overlay').click( function () {
+                    pictureBox.close();
                 });
-                $('.picture-box .image').click(function (event) {
+                $('.picture-box .image-wrapper').click( function () {
+                    pictureBox.close();
+                });
+                $('.picture-box .image').click( function (event) {
                     event.stopPropagation();
                 });
             };
+
+            function loadImageDimensions () {
+                pictureBox.image.actual.width = pictureBox.image.element.width();
+                pictureBox.image.actual.height = pictureBox.image.element.height();
+                pictureBox.image.display = pictureBox.image.actual;
+            }
+
+            function loadWrapperDimensions () {
+                pictureBox.wrapper.width = pictureBox.wrapper.element.width();
+                pictureBox.wrapper.height = pictureBox.wrapper.element.height();
+            }
+
+            function autoResize() {
+                while ((pictureBox.image.display.width > pictureBox.wrapper.width) ||
+                    (pictureBox.image.display.height > pictureBox.wrapper.height)) {
+                    var success = zoomOut();
+                    if(!success){ //max zoom reached
+                        return;
+                    }
+                }
+            }
+
+            function zoomIn(){
+                if (pictureBox.zoom < ZOOM_MAX){
+                    pictureBox.zoom++;
+                    applyZoom();
+                    return true;
+                }
+                return false;
+            }
+
+            function zoomOut(){
+                if (pictureBox.zoom > ZOOM_MIN) {
+                    pictureBox.zoom--;
+                    applyZoom();
+                    return true;
+                }
+                return false;
+            }
+
+            function applyZoom () {
+                var adjustment = pictureBox.image.actual.width * (pictureBox.zoom/10);
+                pictureBox.image.display.width = pictureBox.image.actual.width + adjustment;
+                adjustment = pictureBox.image.actual.height * (pictureBox.zoom/10);
+                pictureBox.image.display.height = pictureBox.image.actual.height + adjustment;
+                $scope.$apply();
+            }
+
+            function isLandscape () {
+                return (pictureBox.image.actual.width > pictureBox.image.actual.height);
+            }
         },
         link: function postLink (scope, element) {
-            scope.pictureBox.image = $('img', element);
-            $(scope.pictureBox.image).click(scope.pictureBox.open);
+            var image = $('img', element);
+            $(image).click(scope.pictureBox.open);
 
             //create modal html
-            var imgSrc = scope.pictureBox.image.attr('src');
-            scope.pictureBox.popover = '<div class="picture-box overlay">' +
-                '<div class="image-wrapper"><i class="glyphicon glyphicon-remove close-icon"></i>' +
+            var imgSrc = image.attr('src');
+            var html = '<div class="picture-box overlay">' +
+                '<i class="glyphicon glyphicon-remove close-icon"></i>' +
+                '<div class="image-wrapper">' +
                 '<div class="image-wrapper-inner">' +
-                '<img class="image" src="'+imgSrc+'"></div></div></div>';
+                '<img class="image" src="'+imgSrc+'" width="{{pictureBox.image.display.width}}" height="{{pictureBox.image.display.height}}">' +
+                '</div></div></div>';
+            scope.pictureBox.popover = $compile(html)(scope);
         }
     };
 });
